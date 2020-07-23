d92a0e7b3822d05cd983d967ee02bbaf2852f9ed
mertsev <mse1996@gmail.com>
2020-07-23 17:57:09 +0300

    end of the day commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,19 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { rootReducers } from '../reducers';
+import { rootSaga } from '../sagas';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+const sagaMiddleware = createSagaMiddleware();
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__']) ||
+  compose;
+
+export const store = createStore(
+  rootReducers,
+  {},
+  composeEnhancer(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
