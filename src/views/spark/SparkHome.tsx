be315d4e0e9c8ef5e74f4fbcb199819c47e3d294
mertsev <mse1996@gmail.com>
2020-08-13 18:28:02 +0300

    Spark inn data additions

--- src/views/spark/SparkHome.tsx
+++ src/views/spark/SparkHome.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import NavBar from '../../components/navbar';
 import { Button, styled, Grid, Paper } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import './SparkHome.css';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -42,8 +43,42 @@ function SparkHome() {
     <Grid container spacing={3}>
       <Grid item xs={8}>
         <Paper className={classes.paper}>
-          <div className="companyName">ПУБЛИЧНОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО "НЕФТЯНАЯ КОМПАНИЯ "РОСНЕФТЬ"</div>
-          <div className="companyStatus" color="lightGreen">Действующая организация</div>
+          <div className="companyName"><h3>ПУБЛИЧНОЕ АКЦИОНЕРНОЕ ОБЩЕСТВО "НЕФТЯНАЯ КОМПАНИЯ "РОСНЕФТЬ"</h3></div>
+          <div className="companyStatus" color="lightGreen"><p>Действующая организация</p></div>
+          <dl className="companyCol">
+            <dt className="company-info__title">ОГРН</dt>
+            <dd className="company-info__text">
+              <span id="clip_ogrn">1027700043502</span>
+            </dd>
+            <dd className="company-info__text">
+              <span id="clip_ogrn"> от 19 июля 2002 г.</span>
+            </dd>
+          </dl>
+          <dl className="companyCol">
+            <dt className="company-info__title">ИНН/КПП</dt>
+            <dd className="company-info__text">
+              <span id="clip_inn">7706107510</span>
+            </dd>
+            <dd className="company-info__text">
+              <span id="clip_kpp">770601001</span>
+            </dd>
+          </dl>
+          <dl className="companyCol">
+            <dt className="company-info__title">Дата регистрации</dt>
+            <dd className="company-info__text">
+              <span id="foundingDate">07.06.1993</span>
+            </dd>
+            <dt className="company-info__title">Уставный капитал</dt>
+            <dd className="company-info__text">
+              <span id="foundingDate">105 981 778,17 руб.</span>
+            </dd>
+          </dl>
+          <dt className="company-info__title">Юридический адрес</dt>
+          <dd className="company-info__text">
+              <span itemProp="address">
+                
+              </span>
+            </dd>
         </Paper>
       </Grid>
       <Grid item xs={12} sm={6}>
