5962c30f81300debaedca5224c09ac53d5f0b30c
mertsev <mse1996@gmail.com>
2020-08-14 13:46:27 +0300

    Updates

---
+++ precache-manifest.50a52d326b5dd0a43d9d984e100c3a4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4d9c6d3d22914a672a1989a741f363",
+    "url": "/react-test-project/index.html"
+  },
+  {
+    "revision": "f80dc9b165b436aa87d8",
+    "url": "/react-test-project/static/css/main.c47cf449.chunk.css"
+  },
+  {
+    "revision": "f4547d706cddb1e7a114",
+    "url": "/react-test-project/static/js/2.2e8abe1c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-test-project/static/js/2.2e8abe1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80dc9b165b436aa87d8",
+    "url": "/react-test-project/static/js/main.9cbf53f4.chunk.js"
+  },
+  {
+    "revision": "bde8089adc2c74b0bd9e",
+    "url": "/react-test-project/static/js/runtime-main.fae0572c.js"
+  }
+]);
